fa96dbae36be43feed04076e432fa4231f2173d3
Vadym Khodak <32848370+VadymKhodak@users.noreply.github.com>
2021-10-03 17:58:28 +0300

    Add FastAPI to python client to consume grpc

---
+++ client/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+ORIGINS = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:80",
+    "http://localhost:8000",
+    os.getenv("ADDITIONAL_ORIGIN", "http://localhost")
+]
+SERVER_HOST = os.getenv("SERVER_HOST", "localhost")
+SERVER_PORT = os.getenv("SERVER_PORT", "50051")
